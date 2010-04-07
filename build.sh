d3e91abf6d443c2313a23e1bd99984f0086be1cd
Alexander Strange <astrange@ithinksw.com>
2010-04-07 15:08:54 -0400

    Update to local copy with random changes.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-CC=gcc
+CC=gcc-4.2
 CFLAGS="-Os -march=pentium -mtune=generic -std=gnu99"
 
 $CC $CFLAGS -o tripper2ch tripper.c
 $CC $CFLAGS -DSHIICHAN4K -o trippershii tripper.c
 $CC $CFLAGS -DWAKABARC4 -o tripperc4 tripper.c
-$CC $CFLAGS -o 2chdict tdict.c
+$CC $CFLAGS -o 2chdict tdict.c || true
