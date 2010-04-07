a9e5247e65f44addba8f32a543b080de7fc5cc4c
mrvacbob <mrvacbob@users.noreply.github.com>
2010-04-07 17:56:30 -0400

    Rename secure tripcode modes to just SHIICHAN/WAKABA

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ CC=gcc
 CFLAGS="-O3 -g -std=gnu99"
 
 $CC $CFLAGS -o tripper2ch tripper.c
-$CC $CFLAGS -DSHIICHAN4K -o trippershii tripper.c
-$CC $CFLAGS -DWAKABARC4 -o tripperc4 tripper.c
+$CC $CFLAGS -DSHIICHAN -o trippershii tripper.c
+$CC $CFLAGS -DWAKABA -o tripperc4 tripper.c
