926c1c5eb7c54fd3c789e5a0b6e4fb90a70fa02c
Alexander Strange <astrange@ithinksw.com>
2010-04-07 15:11:13 -0400

    Remove tdict.c for simplicity

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ CFLAGS="-O3 -g -std=gnu99"
 $CC $CFLAGS -o tripper2ch tripper.c
 $CC $CFLAGS -DSHIICHAN4K -o trippershii tripper.c
 $CC $CFLAGS -DWAKABARC4 -o tripperc4 tripper.c
-$CC $CFLAGS -o 2chdict tdict.c || true
